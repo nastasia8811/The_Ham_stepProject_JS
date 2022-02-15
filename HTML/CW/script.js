eaa7094f58e96bc42bece84832f0fe0419b578cc
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-02-15 21:53:00 +0200

    cw

---
+++ HTML/CW/script.js
@@ -0,0 +1,28 @@
+// const myNumber = 1;
+// console.log(myNumber);
+//
+//
+// let myString = '123';
+// console.log(typeof myString);
+// console.log(typeof parseInt(myString));
+// console.log(typeof parseFloot(myString));
+// console.log(typeof + myString);
+//
+//
+// alert ("hello!This is alert");
+
+
+// const myNumberOne = +prompt('введите номер 1');
+// const myNumberTwo = +prompt('введите номер 2');
+// console.log(myNumberOne + myNumberTwo);
+// console.log(myNumberOne - myNumberTwo);
+// console.log(myNumberOne / myNumberTwo);
+// console.log(myNumberOne % myNumberTwo);
+
+
+
+if(c>1 && c<10) {
+    console.log(c);
+}
+
+
