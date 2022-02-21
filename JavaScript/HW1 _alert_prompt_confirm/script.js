0b954640be573c6a5d8747f092c14d84cf237ce9
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-02-22 01:23:02 +0200

    HW 6

--- JavaScript/HW1 _alert_prompt_confirm/script.js
+++ JavaScript/HW1 _alert_prompt_confirm/script.js
@@ -1,18 +1,16 @@
 "use strict";
-let userName;
-while (!userName) {
-    userName = prompt("Введите Ваше имя");
-}
-let userAge;
-userAge = prompt("Введите Ваш возраст");
-if (userAge < 18) {
-    alert("You are not allowed to visit this website")
-}else if (userAge >= 18 || userAge <= 22) {
-    (confirm("Are you sure you want to continue?"))
-     alert(`Welcome, ${userName}`)
-}else alert("You are not allowed to visit this website")
+const userName = prompt("Введите Ваше имя");
+let userAge = +prompt("Введите Ваш возраст");
 
-// if (userAge >= 23) {
-//     alert(`Welcome, ${userName}`)
-// }
+if (userAge < 17) {
+    alert("You are not allowed to visit this website");
+}
+else if (userAge > 18 && userAge < 22) {
+    (confirm("Are you sure you want to continue?"));
+        alert(`Welcome, ${userName}`);
+}else {
+    alert("You are not allowed to visit this website");
+}
+
+
 
