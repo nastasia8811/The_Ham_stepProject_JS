6e85084b6fe93f1a2e7905e04c8ffc8cbc31aa8c
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-02-21 12:08:48 +0200

    cw JS

--- JavaScript/HW1 _alert_prompt_confirm/script.js
+++ JavaScript/HW1 _alert_prompt_confirm/script.js
@@ -7,11 +7,12 @@ let userAge;
 userAge = prompt("Введите Ваш возраст");
 if (userAge < 18) {
     alert("You are not allowed to visit this website")
-}else if (userAge >= 18 || userAge <= 22)) {
-    (confirm(`Welcome, ${userName}`))
+}else if (userAge >= 18 || userAge <= 22) {
+    (confirm("Are you sure you want to continue?"))
+     alert(`Welcome, ${userName}`)
 }else alert("You are not allowed to visit this website")
 
-if (userAge >= 23) {
-    alert(`Welcome, ${userName}`)
-}
+// if (userAge >= 23) {
+//     alert(`Welcome, ${userName}`)
+// }
 
