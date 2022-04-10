e30dd84fa1bd3eda00aec11802db975ce846914e
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-10 18:25:31 +0200

    JS . My Home work 5

--- JavaScript/HW5_JS/script.js
+++ JavaScript/HW5_JS/script.js
@@ -4,12 +4,14 @@ const createNewUser = (firstName= null,lastName= null) => {
         lastName,
         get getLogin() {
             return `${this.firstName.slice(0,1).toLowerCase()}${this.lastName.toLowerCase()}`
-        }
+        },
     };
 };
 let newUser = createNewUser(prompt("Enter your firstName"),prompt("Enter your lastName"));
 console.log(newUser.getLogin);
 console.log(newUser);
+
+
 
 
 
