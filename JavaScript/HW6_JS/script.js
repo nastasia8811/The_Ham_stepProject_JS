784ec0d1dbca5cf07bd057d972efd550024641f1
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-14 22:59:04 +0200

    JS . My Home work 6

--- JavaScript/HW6_JS/script.js
+++ JavaScript/HW6_JS/script.js
@@ -1,28 +1,38 @@
 
-let createNewUser = (firstName= null,lastName= null) => {
+let createNewUser = (firstName= null,lastName= null,) => {
     return {
         firstName,
         lastName,
         get getLogin() {
             return `${this.firstName.slice(0,1).toLowerCase()}${this.lastName.toLowerCase()}`
         },
-        getAge: function() {
-            let today = new Date(this.getDate().getMonth().getFullYear())
-            return today - this.birthday
+         getAge: function () {
+            let today = new Date();
+            const dd = String(today.getDate()).padStart(2, '0');
+            const mm = String(today.getMonth() + 1).padStart(2, '0');
+            const yyyy = today.getFullYear();
+            today = mm + '.' + dd + '.' + yyyy;
+            return today - newUser.birthday;
         },
-        get getPassword(){
+        get getPassword() {
             return `${this.firstName.slice(0,1).toUpperCase()}${this.lastName.toLowerCase()}${this.birthday.slice(6)}`
         },
 };
 };
+
 let newUser = createNewUser(prompt("Enter your firstName"),prompt("Enter your lastName"));
+
+newUser.getAge();
+
 newUser.birthday = new Date(prompt("Enter your birthday",""));
 const dd = String(newUser.birthday.getDate()).padStart(2, '0');
 const mm = String(newUser.birthday.getMonth() + 1).padStart(2, '0');
 const yyyy = newUser.birthday.getFullYear();
 newUser.birthday = mm + '.' + dd + '.' + yyyy;
 
+console.log(newUser);
 console.log(newUser.birthday);
 console.log(newUser.getLogin);
-console.log(newUser);
 console.log(newUser.getPassword);
+console.log(newUser.getAge);
+
