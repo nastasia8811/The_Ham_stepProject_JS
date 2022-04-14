d89852c7ff1a9ca743f5b1d6d814af646c62bdb7
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-14 15:48:26 +0200

    JS . My Home work 6

--- JavaScript/HW6_JS/script.js
+++ JavaScript/HW6_JS/script.js
@@ -6,7 +6,14 @@ let createNewUser = (firstName= null,lastName= null) => {
         get getLogin() {
             return `${this.firstName.slice(0,1).toLowerCase()}${this.lastName.toLowerCase()}`
         },
-    };
+        getAge: function() {
+            let today = new Date(this.getDate().getMonth().getFullYear())
+            return today - this.birthday
+        },
+        get getPassword(){
+            return `${this.firstName.slice(0,1).toUpperCase()}${this.lastName.toLowerCase()}${this.birthday.slice(6)}`
+        },
+};
 };
 let newUser = createNewUser(prompt("Enter your firstName"),prompt("Enter your lastName"));
 newUser.birthday = new Date(prompt("Enter your birthday",""));
@@ -18,13 +25,4 @@ newUser.birthday = mm + '.' + dd + '.' + yyyy;
 console.log(newUser.birthday);
 console.log(newUser.getLogin);
 console.log(newUser);
-
-
-newUser.getAge = function() {
-    const now = new Date(); //Текущя дата
-    const today = new Date(now.getDate(),now.getMonth(),now.getFullYear()); //Текущя дата без времени
-    const dob = new Date(newUser.birthday); //Дата рождения
-    let age = getAge.today - newUser.birthday;
-};
-newUser.getAge()
-console.log(createNewUser(newUser.getAge));
+console.log(newUser.getPassword);
