269c79dd23f5fbed823db4249ef34954ea6ccc39
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-16 23:27:09 +0200

    JS . My Home work 7

---
+++ JavaScript/HW7_JS/script.js
@@ -0,0 +1,14 @@
+// forEach - перебирає елементи масиву, один раз для кожного елементу масиву
+//arк.length = 0;
+//typeOf
+
+
+function filterBy(testArr,string){
+}
+filterBy(testArr = [454,1111,"gjgjgj",55555,"fhff"], "jffjgfk");
+let newArray = testArr.filter(function(elem) {
+    if (!isNaN(elem)) {
+        return arguments;
+    }
+});
+console.log(newArray);
