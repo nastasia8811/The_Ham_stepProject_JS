ea11f0f7f983630fd608a40ef0eea432a87d3809
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-17 23:10:28 +0200

    JS . My Home work 6

--- JavaScript/HW6_JS/script.js
+++ JavaScript/HW6_JS/script.js
@@ -7,13 +7,12 @@ let createNewUser = (firstName= null,lastName= null,) => {
             return `${this.firstName.slice(0,1).toLowerCase()}${this.lastName.toLowerCase()}`
         },
          getAge: function () {
-            let today = new Date();
-            const dd = String(today.getDate()).padStart(2, '0');
-            const mm = String(today.getMonth() + 1).padStart(2, '0');
-            const yyyy = today.getFullYear();
-            today = mm + '.' + dd + '.' + yyyy;
-            return today - newUser.birthday;
+
+            return dateToday - newUser.birthday;
         },
+
+
+
         get getPassword() {
             return `${this.firstName.slice(0,1).toUpperCase()}${this.lastName.toLowerCase()}${this.birthday.slice(6)}`
         },
@@ -22,17 +21,32 @@ let createNewUser = (firstName= null,lastName= null,) => {
 
 let newUser = createNewUser(prompt("Enter your firstName"),prompt("Enter your lastName"));
 
-newUser.getAge();
+
+function addLeadingZero (d){
+    return (d<10) ? "0" + d : d;
+};
+addLeadingZero()
+
+function dateToday (today = new Date()) {
+    const dd = addLeadingZero(today.getDate());
+    const mm = addLeadingZero(today.getMonth() + 1);
+    const yyyy = today.getFullYear();
+
+    return `${dd}.${mm}.${yyyy}`
+}
+dateToday();
+console.log(dateToday());
 
 newUser.birthday = new Date(prompt("Enter your birthday",""));
-const dd = String(newUser.birthday.getDate()).padStart(2, '0');
-const mm = String(newUser.birthday.getMonth() + 1).padStart(2, '0');
+const dd = addLeadingZero(newUser.birthday.getDate());
+const mm = addLeadingZero(newUser.birthday.getMonth() + 1);
 const yyyy = newUser.birthday.getFullYear();
-newUser.birthday = mm + '.' + dd + '.' + yyyy;
+newUser.birthday = (`${dd}.${mm}.${yyyy}`);
 
 console.log(newUser);
 console.log(newUser.birthday);
 console.log(newUser.getLogin);
 console.log(newUser.getPassword);
-console.log(newUser.getAge);
+console.log(newUser.getAge());
+
 
