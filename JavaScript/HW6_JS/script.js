6186eae06ef3432016b02ec941230cde897d097a
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-18 11:37:02 +0200

    JS . My Home work 6

--- JavaScript/HW6_JS/script.js
+++ JavaScript/HW6_JS/script.js
@@ -1,4 +1,3 @@
-
 let createNewUser = (firstName= null,lastName= null,) => {
     return {
         firstName,
@@ -7,42 +6,27 @@ let createNewUser = (firstName= null,lastName= null,) => {
             return `${this.firstName.slice(0,1).toLowerCase()}${this.lastName.toLowerCase()}`
         },
          getAge: function () {
-
-            return dateToday - newUser.birthday;
+            let today = new Date()
+                 const dd = addLeadingZero(today.getDate());
+                 const mm = addLeadingZero(today.getMonth() + 1);
+                 const yyyy = today.getFullYear();
+             return today - isNaN(this.birthday);
         },
-
-
-
         get getPassword() {
             return `${this.firstName.slice(0,1).toUpperCase()}${this.lastName.toLowerCase()}${this.birthday.slice(6)}`
         },
 };
 };
-
 let newUser = createNewUser(prompt("Enter your firstName"),prompt("Enter your lastName"));
-
-
 function addLeadingZero (d){
     return (d<10) ? "0" + d : d;
-};
-addLeadingZero()
-
-function dateToday (today = new Date()) {
-    const dd = addLeadingZero(today.getDate());
-    const mm = addLeadingZero(today.getMonth() + 1);
-    const yyyy = today.getFullYear();
-
-    return `${dd}.${mm}.${yyyy}`
 }
-dateToday();
-console.log(dateToday());
-
+addLeadingZero()
 newUser.birthday = new Date(prompt("Enter your birthday",""));
 const dd = addLeadingZero(newUser.birthday.getDate());
 const mm = addLeadingZero(newUser.birthday.getMonth() + 1);
 const yyyy = newUser.birthday.getFullYear();
 newUser.birthday = (`${dd}.${mm}.${yyyy}`);
-
 console.log(newUser);
 console.log(newUser.birthday);
 console.log(newUser.getLogin);
