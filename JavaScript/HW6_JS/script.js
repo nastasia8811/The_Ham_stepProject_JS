9238932e3e1bbe5d524d85ccc0a71934500d0666
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-19 13:43:36 +0200

    JS . My Home work 6

--- JavaScript/HW6_JS/script.js
+++ JavaScript/HW6_JS/script.js
@@ -1,36 +1,31 @@
-let createNewUser = (firstName= null,lastName= null,) => {
+let createNewUser = (firstName= null,lastName= null,birthday = null) => {
     return {
         firstName,
         lastName,
-        get getLogin() {
+        birthday,
+        getLogin: function () {
             return `${this.firstName.slice(0,1).toLowerCase()}${this.lastName.toLowerCase()}`
         },
          getAge: function () {
             let today = new Date()
-                 const dd = addLeadingZero(today.getDate());
-                 const mm = addLeadingZero(today.getMonth() + 1);
-                 const yyyy = today.getFullYear();
-             return today - isNaN(this.birthday);
+             let dd = this.birthday.slice(0,2);
+             let mm = this.birthday.slice(3,5);
+             let yyyy = this.birthday.slice(6);
+             let date = today.getFullYear() - yyyy;
+             if (mm > today.getMonth() || (mm === today.getMonth() && dd > today.getDay())){
+                 date--;
+             }
+              return date
         },
-        get getPassword() {
+        getPassword: function () {
             return `${this.firstName.slice(0,1).toUpperCase()}${this.lastName.toLowerCase()}${this.birthday.slice(6)}`
         },
 };
 };
-let newUser = createNewUser(prompt("Enter your firstName"),prompt("Enter your lastName"));
-function addLeadingZero (d){
-    return (d<10) ? "0" + d : d;
-}
-addLeadingZero()
-newUser.birthday = new Date(prompt("Enter your birthday",""));
-const dd = addLeadingZero(newUser.birthday.getDate());
-const mm = addLeadingZero(newUser.birthday.getMonth() + 1);
-const yyyy = newUser.birthday.getFullYear();
-newUser.birthday = (`${dd}.${mm}.${yyyy}`);
-console.log(newUser);
-console.log(newUser.birthday);
-console.log(newUser.getLogin);
-console.log(newUser.getPassword);
+let newUser = createNewUser(prompt("Enter your firstName"),prompt("Enter your lastName"),prompt("Enter your birthday",""));
+console.log(newUser.getLogin());
+console.log(newUser.getPassword());
 console.log(newUser.getAge());
+console.log(newUser);
 
 
