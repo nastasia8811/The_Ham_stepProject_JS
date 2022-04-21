7519f8b76ec55854cacc27a6af2e44d7e1493546
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-21 10:35:19 +0200

    JS . My Home work 7

--- JavaScript/HW7_JS/script.js
+++ JavaScript/HW7_JS/script.js
@@ -1,14 +1,3 @@
-
-// function filterBy(testArr,string){
-// }
-// filterBy(testArr = [454,1111,"gjgjgj",55555,"fhff"], "jffjgfk");
-// let newArray = testArr.filter(function(elem) {
-//     if (!isNaN(elem)) {
-//         return arguments;
-//     }
-// });
-// console.log(newArray);
-
 let filterBy = (elementArr = null,type = null) => {
 return elementArr.filter((item)=>{
     if (item === null){
@@ -20,8 +9,6 @@ return elementArr.filter((item)=>{
     }
     return typeof item !== type
 })
-
-
 }
-console.log(filterBy([135, "nnnbnbnb", 765,{}, null, undefined], 'number'));
+console.log(filterBy([135, "LLLLL", 765,{}, null, undefined], 'number'));
 
