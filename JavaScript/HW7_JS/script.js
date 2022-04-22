9ccc26c653097256bf05d0177ec4334b9610e619
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-04-22 10:06:43 +0200

    JS . My Home work 7

--- JavaScript/HW7_JS/script.js
+++ JavaScript/HW7_JS/script.js
@@ -1,6 +1,14 @@
-let filterBy = (elementArr = null,type = null) => {
-return elementArr.filter((item)=>{
-    if (item === null){
+// 1)  Метод forEach дозволяє послідовно перебрати всі елементи масиву.
+//     Метод у параметрі отримує функцію, що виконається для кожного елемента масиву. У цю функцію можна передавати 3 параметри.
+//     Якщо ці параметри є, то перший автоматично потрапить елемент масиву, другий потрапить його номер у масиві (індекс), а третій - сам масив.
+// 2)  let array = [1,2,3,4,5];
+//     array = [];
+// 3) let myArr = [21,20,98,99]
+//   Array.isArray(myArr)
+
+let filterBy = (elementArr = null, type = null) => {
+    return elementArr.filter((item)=>{
+        if (item === null){
         if (type === "null"){
             return false
         } else {
@@ -8,7 +16,6 @@ return elementArr.filter((item)=>{
         }
     }
     return typeof item !== type
-})
+    })
 }
-console.log(filterBy([135, "LLLLL", 765,{}, null, undefined], 'number'));
-
+console.log(filterBy([123,454, null, undefined, "string"], 'number'));
