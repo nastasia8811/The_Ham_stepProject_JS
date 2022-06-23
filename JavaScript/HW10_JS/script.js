7318f9e923674242ae638d0d5d1e76c29955505d
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-06-23 21:18:22 +0200

    HW_10

--- JavaScript/HW10_JS/script.js
+++ JavaScript/HW10_JS/script.js
@@ -1,20 +1,5 @@
-// const getSelector = document.querySelector('.centered-content');
-// console.log(getSelector);
-//
-// const ulClassTabs = document.querySelectorAll('.tabs>li');
-// const ulClassTabsContent = document.querySelectorAll('.tabs-content>li');
-// console.log(Array.from(ulClassTabs));
-// console.log(Array.from(ulClassTabsContent));
-//
-// for (let i = 0; i < ulClassTabs.length; i++){
-//     if (ulClassTabs[1].dataset
-//     console.log(ulClassTabs[1])
-//}
-
 const tabs = document.querySelector('.tabs');
-tabs.addEventListener("click", fTabs);
-
-function fTabs(event){
+tabs.addEventListener("click",(event) =>{
     if (event.target.classList.contains('tabs-title')){
         const dataTab = event.target.dataset.menu;
        const tabHeader = document.querySelectorAll(".tabs-title");
@@ -23,19 +8,16 @@ function fTabs(event){
        }
        event.target.classList.add('active');
        const tabContent = document.querySelectorAll(".tabs-content li");
-
         for (let i = 0; i < tabContent.length; i++) {
             tabContent[i].classList.remove('active');
             const tabLi = tabContent[i].dataset.content;
             if (tabLi === dataTab){
                 tabContent[i].classList.add('active');
-            } else {tabContent[i].classList.remove('active');}
-
-
+            } else {tabContent[i].classList.remove('active');
+            }
         }
     }
-
-}
+})
 
 
 
