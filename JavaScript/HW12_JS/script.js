229a58642e98d464032917040a31e505c7a99999
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-06-27 21:59:59 +0200

    HW_12

--- JavaScript/HW12_JS/script.js
+++ JavaScript/HW12_JS/script.js
@@ -1,26 +1,52 @@
+//
+//
+// const letter = document.querySelector('.btn-wrapper');
+// const buttonColor = document.querySelector('.btn');
+//
+//
+// letter.addEventListener('keydown',(event) => {
+//     if (event.key.classList.includes('btn')) {
+//         const dataLetter = event.key.dataset.letter;
+//         const wrapper = document.querySelectorAll('.btn-wrapper');
+//         for (let i = 0; i < wrapper.length; i++) {
+//             wrapper[i].classList.style.background('.btn.active');
+//             event.key.classList.add('active');
+//             console.log(event.target)
+//         }
+//
+//         for (let i = 0; i < letter.length; i++) {
+//             letter[i].classList.remove('active');
+//             const dataContent = buttonColor[i].dataset.content
+//             if (event.key === dataContent) {
+//                 buttonColor[i].classList.add('active');
+//             }
+//             buttonColor[i].classList.remove('active');
+//
+//         }}
+//     })
 
+const displayBtns = document.querySelectorAll(".btn");
+console.log(displayBtns);
 
-const letter = document.querySelector('.btn-wrapper');
-const buttonColor = document.querySelector('.btn');
-
-
-letter.addEventListener('keydown',(event) => {
-    if (event.key.classList.includes('btn')) {
-        const dataLetter = event.key.dataset.letter;
-        const wrapper = document.querySelectorAll('.btn-wrapper');
-        for (let i = 0; i < wrapper.length; i++) {
-            wrapper[i].classList.style.background('.btn.active');
-            event.key.classList.add('active');
-            console.log(event.target)
-        }
-
-        for (let i = 0; i < letter.length; i++) {
-            letter[i].classList.remove('active');
-            const dataContent = buttonColor[i].dataset.content
-            if (event.key === dataContent) {
-                buttonColor[i].classList.add('active');
+// function scanBtns() {
+//     document.addEventListener("keydown", (event) => {
+//         displayBtns.forEach(element => {
+//             element.style.backgroundColor = "black";
+//             if(event.key === element.textContent || event.key === (element.textContent).toLowerCase()) {
+//                 element.style.backgroundColor = "red";
+//             }
+//         })
+//     });
+// }
+// scanBtns();
+function scanBtns() {
+    document.addEventListener("keydown", (event) => {
+        displayBtns.forEach(element => {
+            element.style.backgroundColor = "black";
+            if(event.key === element.textContent || event.key === (element.textContent).toLowerCase()) {
+                element.style.backgroundColor = "red";
             }
-            buttonColor[i].classList.remove('active');
-
-        }}
-    })
+        })
+    });
+}
+scanBtns();
