8b557162b8ff833d4049b14aa378aba4fd311369
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-07-11 15:57:34 +0200

    14

--- JavaScript/HW142_JS/script.js
+++ JavaScript/HW142_JS/script.js
@@ -1,6 +1,19 @@
 'use strict';
-let button = document.body.querySelector('#color-theme');
-button.addEventListener('click', function (){
-    document.body.classList.toggle("yellow-theme");
-    localStorage.setItem("new","yellow");
-});
+
+let state = true;
+
+document.body.style.backgroundColor = localStorage.getItem('bgcolor') || 'yellow';
+
+function change(){
+    if (state){
+        document.body.style.backgroundColor = "red";
+        document.getElementById('color-theme').style.color='blue';
+        localStorage.setItem('bgcolor', 'red');
+
+    } else {
+        document.body.style.backgroundColor = "#005fff";
+        document.getElementById('color-theme').style.color='lightgreen';
+        localStorage.setItem('bgcolor', '#005fff');
+    }
+    state = !state;
+}
