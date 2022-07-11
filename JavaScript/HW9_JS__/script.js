a06f00383b4b4c647cfc609d70329979f067f74a
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-07-11 16:15:02 +0200

    9

--- JavaScript/HW9_JS__/script.js
+++ JavaScript/HW9_JS__/script.js
@@ -1,37 +1,10 @@
 
-function getNewList(arr = [],parent = document.body) {
-    arr.map((elem) => {`<li>${elem}</li>`
-        document.innerText = elem;
-        return li
+function getNewList(arr =[] ,parent = document.body) {
 
+    arr.map(elem => {
+        let li = document.createElement('li');
+        li.innerText = elem;
+        parent.append(li);
     })
-
-    parent.append(li);
-    }
-    getNewList(["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"], document.querySelector('ul'));
-
-
-// Создайте в html ul, дайте класс ul
-// Потом в аргумент передавайте
-// document. qwerySelector(“ul”)
-//
-// 3 строчку убрать
-//
-// Потом пишете
-// arr. map
-// В каждом цикле создаёте li и устанавливаете innerText = elem, а потом
-// parent( он же второй аргумент)
-// parent.append( li)
-
-// 2 вариант
-
-// function getNewList(arr = []) {
-//     const ulList = document.createElement("ul");
-//     const newArr = arr.map((elem) => {
-//         const li = `<li>${elem}</li>`
-//         return li
-//     });
-//     ulList.innerHTML = newArr;
-//     return ulList
-// }
-// document.body.after(getNewList( ["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"]));
+}
+getNewList(["hello", "world", "Kiev", "Kharkiv", "Odessa", "Lviv"], document.querySelector('ul'));
