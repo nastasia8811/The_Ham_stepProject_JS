7b032f5e1edf2722bde1533ae4aacac79500c7cd
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-07-26 15:10:06 +0200

    HW2_gulp-scss

--- advance_html_css/HW2_gulp-scss/gulpfile.js
+++ advance_html_css/HW2_gulp-scss/gulpfile.js
@@ -6,6 +6,6 @@ import browserSync from 'browser-sync';
 const sass = gulpSass(dartSass);
 const bs = browserSync.create();
 
-const buildStyles = () => gulp.src('./src/styles/**/*.scss')
+export const buildStyles = () => gulp.src('./src/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
