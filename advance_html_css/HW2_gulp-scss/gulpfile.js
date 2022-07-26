65f37f38c71bbd13b1298e5eac1f554ca05ae11c
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-07-26 20:47:02 +0200

    HW2_gulp-scss

--- advance_html_css/HW2_gulp-scss/gulpfile.js
+++ advance_html_css/HW2_gulp-scss/gulpfile.js
@@ -1,11 +1,37 @@
-import gulp from 'gulp';
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import browserSync from 'browser-sync';
+// import gulp from 'gulp';
+// import dartSass from 'sass';
+// import gulpSass from 'gulp-sass';
+// import browserSync from 'browser-sync';
+//
+// const sass = gulpSass(dartSass);
+// const bs = browserSync.create();
+//
+// export const buildStyles = () => gulp.src('./src/styles/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./dist/css'));
 
-const sass = gulpSass(dartSass);
-const bs = browserSync.create();
+const { src, dest, watch, series } = require("gulp");
 
-export const buildStyles = () => gulp.src('./src/styles/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
+const moveCSS = () => {
+    return src("./src/styles/**/*.scss")
+        .pipe(dest("./dist/css/"));
+}
+
+const moveIMG = () => {
+    return src("./src/images/**/*.jpg")
+        .pipe(dest("./dist/images/"));
+}
+
+const watchers = () => {
+    watch('./src/styles/**/*.scss', moveCSS);
+    watch('./src/images/**/*.jpg', moveIMG);
+}
+
+const dev = series(moveCSS, moveIMG, watchers);
+
+// gulp.task("watchers", watchers);
+exports.watchers = watchers;
+exports.moveCSS = moveCSS;
+exports.moveIMG = moveIMG;
+exports.dev = dev;
+
