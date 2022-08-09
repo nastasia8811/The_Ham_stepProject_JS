4e91225a84cc26d6dd9a72aa20b7f1990388e974
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-08-09 20:59:12 +0200

    hw 2 - 09/08/2022 20:58

--- advance_html_css/HW2_root/gulpfile.js
+++ advance_html_css/HW2_root/gulpfile.js
@@ -58,39 +58,102 @@
 //     );
 //
 
-const { src, dest, watch, series } = require("gulp");
+// const { src, dest, watch, series } = require("gulp");
+//
+//
+// const moveCSS = () => {
+//     return src("./src/styles/**/*.scss")
+//         .pipe(dest("./dist/css/"));
+// }
+//
+// const moveIMG = () => {
+//     return src("./src/images/**/*.jpg")
+//         .pipe(dest("./dist/images/"));
+// }
+//
+// const moveJs = () => {
+//     return src("./src/js/**/*.js")
+//         .pipe(dest("./dist/js/"))
+// }
+//
+// const watchers = () => {
+//     watch('./src/css/*.css', moveCSS);
+//     watch('./src/image/**/*.jpg', moveIMG);
+//     watch('./src/js/**/*js', moveJs);
+// }
+//
+// const dev = series(moveCSS, moveIMG, moveJs, watchers);
+//
+// // gulp.task("watchers", watchers);
+// exports.watchers = watchers;
+// exports.moveCSS = moveCSS;
+// exports.moveIMG = moveIMG;
+// exports.moveJs = moveJs;
+// exports.dev = dev;
 
 
-const moveCSS = () => {
-    return src("./src/styles/**/*.scss")
-        .pipe(dest("./dist/css/"));
+
+'usr strict'
+const { parallel, series, watch , src, dest} = require('gulp');
+const bs = require('browser-sync').create();
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require('gulp-autoprefixer');
+// const gulpClean = require('gulp-clean');
+const cleanCss = require('gulp-clean-css');
+const concat = require('gulp-concat');
+// const imagemin = import('gulp-imagemin')
+// const imagemin = require ('gulp-imagemin');
+const jsMin = require ('gulp-js-minify');
+const gulpUglify = require ("gulp-uglify");
+
+const serv =() => {
+    bs.init({
+        server:{
+            baseDir:"./",
+        },
+        open: true,
+    });
+};
+
+const scripts = (cb) => {
+    src("./src/js/*.js")
+        .pipe(concat('scripts.min.js'))
+        .pipe(jsMin())
+        .pipe(gulpUglify())
+        .pipe(dest("dist/js/*.js"))
+        .pipe(bs.stream());
+    cb();
 }
 
-const moveIMG = () => {
-    return src("./src/images/**/*.jpg")
-        .pipe(dest("./dist/images/"));
+const styles =(cback) => {
+    src("./src/styles/**/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(concat('all.css'))
+        .pipe(cleanCss())
+        .pipe(dest("dist/css")).pipe(bs.stream())
+    cback();
 }
 
-const moveJs = () => {
-    return src("./src/js/**/*.js")
-        .pipe(dest("./dist/js/"))
-}
+const images = (cb) => {
+    src("./src/images/**/*.{jpg,jpeg,png,svg,webp}")
+        // .pipe(imagemin())
+        .pipe(dest("./dist/images"));
+    cb()
+};
 
-const watchers = () => {
-    watch('./src/css/*.css', moveCSS);
-    watch('./src/image/**/*.jpg', moveIMG);
-    watch('./src/js/**/*js', moveJs);
-}
 
-const dev = series(moveCSS, moveIMG, moveJs, watchers);
+const watcher =()=> {
+    watch("*html").on("change", bs.reload);
+    watch("./src/js/*.js").on("change", series(scripts, bs.reload));
+    watch("./src/styles/**/*.scss", styles);
+    watch("./src/images/**/*.{jpg,jpeg,png,svg,webp}").on(
+        "change",
+        series(images, bs.reload)
+    );
+};
 
-// gulp.task("watchers", watchers);
-exports.watchers = watchers;
-exports.moveCSS = moveCSS;
-exports.moveIMG = moveIMG;
-exports.moveJs = moveJs;
-exports.dev = dev;
-
+exports.default = parallel(serv, watcher, series(styles, images, scripts));
 
 
 
