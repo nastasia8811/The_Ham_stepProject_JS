1c7638f0967cd1356732a6bad2b5111f7356d56d
Anastasiia Melnyk <nastasia8811@gmail.com>
2022-08-17 18:56:33 +0200

    N2

--- advance_html_css/HW2_root/src/js/script.js
+++ advance_html_css/HW2_root/src/js/script.js
@@ -2,6 +2,7 @@ let menuButton = document.querySelector(".header__mobile-menu");
 let closeButton = document.querySelector(".header__mobile-menu_close");
 let openButton = document.querySelector(".header__mobile-menu_open");
 let menuOpen = document.querySelector('.header__navigation');
+let link = document.querySelector('.instagram__wrapper-link');
 
 menuButton.addEventListener('click', () => {
     if (closeButton.classList.contains('active')) {
@@ -17,10 +18,16 @@ menuButton.addEventListener('click', () => {
     }
 });
 
-let mediaQuery = window.matchMedia('(min-width: variables.$M-middle-f)');
+let mediaQuery = window.matchMedia('min-width: variables.$M-middle-f');
 if (mediaQuery.matches) {
     menuOpen.classList.remove('hide');
+    menuOpen.classList.add('active');
 }
+
+// let mediaLarge = link.matchMedia("min-width: variables.$L-large");
+// if (mediaQuery.matches) {
+//     menuOpen.classList.remove('hide');
+
 // let window = document.querySelector('body');
 // window.addEventListener('click', (event) => {
 //     if (!event.target.matches('header__mobile-menu')) {
