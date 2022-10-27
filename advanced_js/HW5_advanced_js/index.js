47832d2747d1a64362a36e064b92ef2aa29464ae
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-10-27 15:15:09 +0200

    HW5

--- advanced_js/HW5_advanced_js/index.js
+++ advanced_js/HW5_advanced_js/index.js
@@ -1,7 +1,7 @@
 
 
 class Card {
-    constructor(title, body, username, name, email, id, userId) {
+    constructor(title, body, username, name, email, id) {
         this.title = title;
         this.body = body;
         this.username = username;
@@ -10,40 +10,17 @@ class Card {
         this.container = document.createElement('div');
         this.deleteButton = document.createElement("button");
         this.id = id;
-        this.userId = userId;
-    }
-
-    twitterPosts() {
-        fetch('https://ajax.test-danit.com/api/json/posts')
-            .then((response) => response.json())
-            .then((userPost) => {
-                const {title, body, id} = userPost.find((userPost) => this.id === this.userId)
-               this.container.insertAdjacentHTML('beforeend', `<h4 class="post_title">${title}</h4> <p class="post_body">${body}</p>`)
-            })
-            .catch((elem) => console.log(elem.message))
-    }
-
-    twitterUsers() {
-        fetch('https://ajax.test-danit.com/api/json/users')
-            .then((response) => response.json())
-            .then((usersArr) => {
-                usersArr.forEach(({name, username, email, userId}) => {
-                    usersArr.find((usersArr) => this.id === this.userId)
-                    this.container.insertAdjacentHTML('beforeend', `<h3 class="post_name">${username}</h3><h3 class="post_name">${name}</h3> <p class="post_email">${email}</p>`)
-                })
-            })
-            .catch((elem) => console.log(elem.message))
     }
 
     createElements() {
 
-        //this.container.insertAdjacentHTML('beforeend', ` <h4 class="post_title">${this.title}</h4> <p class="post_body">${this.body}</p> `)
+        this.container.insertAdjacentHTML('beforeend', ` <h4 class="post_title">${this.title}</h4> <p class="post_body">${this.body}</p> `)
         this.container.className = 'post';
         document.body.append(this.container);
         const usersInfo = document.createElement('div');
         usersInfo.className = 'user';
 
-        //this.container.insertAdjacentHTML("beforeend", `<h3 class="post_name">${this.username}</h3><h3 class="post_name">${this.name}</h3> <p class="post_email">${this.email}</p>`)
+        this.container.insertAdjacentHTML("beforeend", `<h3 class="post_name">${this.username}</h3><h3 class="post_name">${this.name}</h3> <p class="post_email">${this.email}</p>`)
 
         this.deleteButton.innerHTML = "Delete";
         this.container.append(this.deleteButton);
@@ -61,9 +38,34 @@ class Card {
     render() {
         this.createElements()
 
-        this.twitterPosts()
-        this.twitterUsers()
+
     }
 }
 
 new Card().render()
+
+
+
+
+
+    fetch('https://ajax.test-danit.com/api/json/users')
+        .then((response) => response.json())
+        .then((usersArr) => {
+            usersArr.forEach(({name, username, email, id}) => {
+                fetch('https://ajax.test-danit.com/api/json/posts')
+                    .then((response) => response.json())
+                    .then((userPost) => {
+                        const filterPost = userPost.filter((post) =>  post.userId === id);
+                        console.log(filterPost)
+                        filterPost.forEach(({title, body}) =>
+                            new Card(title, body,name, username, email, id).render()
+                        )
+                        //const {title, body, id} = userPost.find((userPost) => this.id === this.id)
+                        //this.container.insertAdjacentHTML('beforeend', `<h4 class="post_title">${title}</h4> <p class="post_body">${body}</p>`)
+                    })
+                    .catch((elem) => console.log(elem.message))
+                //usersArr.find((usersArr) => this.id === this.userId)
+                // this.container.insertAdjacentHTML('beforeend', `<h3 class="post_name">${username}</h3><h3 class="post_name">${name}</h3> <p class="post_email">${email}</p>`)
+            })
+        })
+        .catch((elem) => console.log(elem.message))
