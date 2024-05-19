5731fde487eb3f6621a19d7bd0bac849c63af8e2
nastasia8811 <101885813+nastasia8811@users.noreply.github.com>
2024-05-19 17:45:23 +0200

    Delete react directory

--- react/react_HW_4/my-app/src/state/appReducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import {reducer as modalReducers} from "./modal/modalReducers"
-import { reducer as productReducer } from "./products/productReducer";
-
-
-export const appReducer = combineReducers({
-    items: productReducer,
-    modals: modalReducers,
-});
--- react/react_HW_5/my-app/src/state/products/actions.js
+++
@@ -1,7 +0,0 @@
-export const CARDS_ART = "CARDS_ART";
-export const ADD_TO_CART = "ADD_TO_CART";
-export const DEL_FROM_CART = "DEL_FROM_CART";
-export const FAVORITES_BUTTON = "FAVORITES_BUTTON";
-export const GET_CARDS = "GET_CARDS";
-export const GET_CARDS_SUCCESS = "GET_CARDS_SUCCESS";
-export const GET_CARDS_ERR = "GET_CARDS_ERR";
